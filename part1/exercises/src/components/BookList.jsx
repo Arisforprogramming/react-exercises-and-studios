e14957f14335a101bb42972f1687d30b1c4e076a
Arisforprogramming <codegarden23@gmail.com>
2024-08-18 14:14:09 -0500

    test commmit

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Make you Smarter Books";
+   let book1 = "https://m.media-amazon.com/images/I/71WWw6d85JL._SL1500_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71jWgemHbML._SL1200_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/61UZS3QA1UL._SL1500_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Intelligent Investor" />
+         <img src={book2} alt="Art Of War!" />
+         <img src={book3} alt="48 Law of power" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,28 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+
+
+// export default function ChoresList () {
+//    return <p>No content is here!</p>;
+// }
+
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+class ChoresList extends React.Component {
+   render() {
+         const chores = ["Gocery Shopping", "Living Room","Doing Dishes","Laundry"];
+      return (
+      <div>                       
+         <h3 className = {classes.choresHeading}>My Chores of the week</h3>
+         <ol>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+            <li className={classes.choresText}>{chores[2]}</li>
+            <li className={classes.choresText}>{chores[3]}</li>
+         </ol>
+      </div>
+      );
+   }
 }
+
+export default ChoresList;
+
